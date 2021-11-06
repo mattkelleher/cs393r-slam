2d1858f39721a283905e018245bb4a01e8a063b6
Francisco Montano <franck11235@gmail.com>
2021-11-06 18:10:15 -0500

    Added Distance and ObserveLaser functions

--- src/slam/slam.cc
+++ src/slam/slam.cc
@@ -58,6 +58,10 @@ SLAM::SLAM() :
     prev_scans_(),
     prev_transforms_() {}
 
+float ParticleFilter::_Distance(Vector2f p1, Vector2f p2) {
+  return sqrt(pow(p1.x() - p2.x(), 2) + pow(p1.y() - p2.y(), 2));
+}
+
 void SLAM::GetPose(Eigen::Vector2f* loc, float* angle) const {
   // Return the latest pose estimate of the robot.
   *loc = Vector2f(0, 0);
@@ -74,6 +78,86 @@ void SLAM::ObserveLaser(const vector<float>& ranges,
   // A new laser scan has been observed. Decide whether to add it as a pose
   // for SLAM. If decided to add, align it to the scan from the last saved pose,
   // and save both the scan and the optimized pose.
+
+  // Initialize starting transform matrix guess T0 with odometry
+  Vector2f loc;
+  float theta;
+
+  // IMPORTANT: Assumed there was a variable called prev_loc and prev_angle
+  GetPose(loc, theta);  // I presume this gives us the current coordinates of the robot
+                        // If not, change
+
+  Matrix3f T0;  
+  float delta_x = loc.x() - prev_loc.x();
+  float delta_y = loc.y() - prev_loc.y();
+  float delta_theta = theta - prev_theta;
+  T0  <<  cos(delta_theta), sin(delta_theta), -loc.x()*cos(delta_theta)-loc.y()*sin(delta_theta),
+          -sin(delta_theta), cos(delta_theta), loc.x()*sin(delta_theta)-loc.y()*cos(delta_theta),
+          0, 0, 1;
+
+  // Initialize T and T_best
+  Matrix3f T, T_best;
+
+  // Initialize scan in current frame
+  const float distance_base2lidar = 0.2; // From assignment 1
+  float phi;
+
+  float x_base2lidar = distance_base2lidar * cos(angle);
+  float y_base2lidar = distance_base2lidar * sin(angle);
+
+  vector<Vector2f> scan, T_scan;
+  float increment = (angle_max - angle_min) / range.size();
+  for (size_t i = 0; i < scan.size(); i++) {
+    phi = angle + (angle_min + i * increment);
+    scan(i) = Vector2f temp_loc(loc.x() + x_base2lidar + range(i)*cos(phi), loc.y() + y_base2lidar) + range(i)*sin(phi);
+  }
+
+  // Initialize randomized error generators
+  // I'm unsure if this is the correct method, but this should sample a normal distribution 
+  std::default_random_engine generator;
+
+  float var = 0.1; // Just a guess
+  std::normal_distribution<float> x_dist(prev_loc.x(),var);
+  std::normal_distribution<float> y_dist(prev_loc.y(),var);
+  std::normal_distribution<float> theta_dist(prev_theta,var);
+
+  // Check odometry to see if pose is sufficiently divergent
+  if ((_Distance(loc, prev_loc) > 0.5) || (abs(theta - prev_theta) > 30*2*M_PI/180)) {
+    for (int N = 1; N < 15; N++) {
+      if (N = 1) {
+        T = T0;
+      } else {
+        // Adding 'error' to Trasformation Matrix  
+        float delta_x = loc.x() - x_dist(generator);
+        float delta_y = loc.y() - y_dist(generator);
+        float delta_theta = theta - theta_dist(generator);
+        
+        T(0,0) = cos(delta_theta);
+        T(0,1) = sin(delta_theta);
+        T(0,2) = -delta_x*cos(delta_theta)-delta_y*sin(delta_theta);
+        T(1,0) = -sin(delta_theta);
+        T(1,1) = cos(delta_theta);
+        T(1,2) = delta_x*sin(delta_theta) -delta_y*cos(delta_theta);
+      }
+
+      for (int M = 1; M <= length(scan), M++) {
+        T_scan(M) = T * scan(i);
+      }
+      for (int M = 1; M <= length(T_scan), M++) {
+        // Read pixel data
+        int x_pixel = floor(raster.width()/2) + floor((prev_loc.x() - T_scan(M).x())/pixel_def);
+        int y_pixel = floor(raster.height()/2) + floor((prev_loc.y() - T_scan(M).y())/pixel_def);
+        prob += raster(x_pixel, y_pixel);
+      }
+      // Normalize probability
+      prob = prob/scan.size();
+
+      if (prob > prob_max) {
+        prob_max = prob;
+        T_best = T;
+      }
+    }
+  }
 }
 
 void SLAM::ObserveOdometry(const Vector2f& odom_loc, const float odom_angle) {
